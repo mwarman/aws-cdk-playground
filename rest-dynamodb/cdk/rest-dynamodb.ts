18acf11b8ec113196e73127602f11083f6edb0b4
Matthew Warman <warmanmr@gmail.com>
2025-05-07 05:31:49 -0400

    prepare cdk

--- rest-dynamodb/cdk/rest-dynamodb.ts
+++ rest-dynamodb/cdk/rest-dynamodb.ts
@@ -1,17 +1,15 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
+
+import { CDK_ENV } from "./utils/config";
 import { RestDynamodbStack } from "./stacks/rest-dynamodb-stack";
 
 const app = new cdk.App();
-new RestDynamodbStack(app, "RestDynamodbStack", {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const stack = new RestDynamodbStack(app, "RestDynamodbStack", {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
+
+cdk.Tags.of(stack).add("App", "cdk-playground.leanstacks.net");
+cdk.Tags.of(stack).add("Env", CDK_ENV);
+cdk.Tags.of(stack).add("OU", "leanstacks");
+cdk.Tags.of(stack).add("Owner", "Matthew Warman");
