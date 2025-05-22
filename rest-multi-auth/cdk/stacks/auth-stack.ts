b5801907ba979c7328e491f561ccce4718ef8922
Matthew Warman <warmanmr@gmail.com>
2025-05-22 07:39:57 -0400

    initial auth stack

--- rest-multi-auth/cdk/stacks/auth-stack.ts
+++ rest-multi-auth/cdk/stacks/auth-stack.ts
@@ -1,8 +1,18 @@
 import * as cdk from "aws-cdk-lib";
+import * as cognito from "aws-cdk-lib/aws-cognito";
 import { Construct } from "constructs";
 
 export class CdkPlaygroundAuthStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const userPool = new cognito.UserPool(this, "CdkPlaygroundUserPool", {
+      userPoolName: "CdkPlaygroundUserPool",
+      featurePlan: cognito.FeaturePlan.LITE,
+      signInAliases: {
+        email: true,
+      },
+      signInCaseSensitive: false,
+    });
   }
 }
